ad01030925b32eaa9f8f1a78030dbe339f925dc8
Foxglade <89978995+Foxglade@users.noreply.github.com>
2021-12-02 14:59:56 +0200

    Add files via upload

---
+++ Banksim2/mainwindow.cpp
@@ -0,0 +1,57 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    objkysy = new creditvaidebit;
+    objmenu = new menu;
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+    ui = nullptr;
+    delete objkysy;
+    objkysy = nullptr;
+    delete objmenu;
+    objmenu = nullptr;
+}
+
+void MainWindow::on_btnKirjaudu_clicked()
+{
+    QJsonObject json;
+    json.insert("username", ui->leKt->text());
+    json.insert("password", ui->leSs->text());
+    QString site_url = "http://localhost:3000/login";
+    QString credentials = "1:1234";
+    QNetworkRequest request((site_url));
+    request.setHeader(QNetworkRequest::ContentTypeHeader, "application/json");
+    QByteArray data = credentials.toLocal8Bit().toBase64();
+    QString headerData = "Basic " + data;
+    request.setRawHeader("Authorization", headerData.toLocal8Bit());
+    loginManager = new QNetworkAccessManager(this);
+    connect(loginManager, SIGNAL(finished (QNetworkReply*)),
+            this, SLOT(loginSlot(QNetworkReply*)));
+    reply = loginManager->post(request, QJsonDocument(json).toJson());
+}
+
+void MainWindow::loginSlot(QNetworkReply *reply)
+{
+    QByteArray response_data = reply->readAll();
+    qDebug() << response_data;
+    if(response_data == "true")
+    {
+        qDebug() << "Oikea tunnus.. avaa form";
+        objkysy->show();
+    }
+    else
+    {
+        ui->leKt->setText("");
+        ui->leSs->setText("");
+        qDebug() << "Väärä tunnus";
+    }
+
+}
